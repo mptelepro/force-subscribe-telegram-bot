0e0619b6cb253d2f54e604a4831b1bd4032c76e5
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 13:12:57 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -4,7 +4,7 @@ class Config():
   ENV = bool(os.environ.get('ENV', False))
   if ENV:
     BOT_TOKEN = os.environ.get("1737141128:AAGjU4R2S5CNY7C_i9UrEg5c2vswqK9f5H4", None)
-    DATABASE_URL = os.environ.get("postgres://eyofrnogltxmwz:9f85e8584199a374615a67bfe18f73edc4afaf9451d16eed3aee5b179708589b@ec2-54-243-195-160.compute-1.amazonaws.com:5432/d325npis91qikn", None)
+    DATABASE_URL = os.environ.get("mongodb+srv://Filterjinnv2hn:Filterjinnv2hn@cluster0.i1rfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", None)
     APP_ID = os.environ.get("2443183", 1741321)
     API_HASH = os.environ.get("66b6799e1b784aff78bad680cd8362db", None)
     SUDO_USERS = list(set(int(x) for x in os.environ.get("1575148105").split()))
@@ -12,7 +12,7 @@ class Config():
     SUDO_USERS = list(set(1575148105))
   else:
     BOT_TOKEN = "1737141128:AAGjU4R2S5CNY7C_i9UrEg5c2vswqK9f5H4"
-    DATABASE_URL = "postgres://eyofrnogltxmwz:9f85e8584199a374615a67bfe18f73edc4afaf9451d16eed3aee5b179708589b@ec2-54-243-195-160.compute-1.amazonaws.com:5432/d325npis91qikn"
+    DATABASE_URL = "mongodb+srv://Filterjinnv2hn:Filterjinnv2hn@cluster0.i1rfo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     APP_ID = "2443183"
     API_HASH = "66b6799e1b784aff78bad680cd8362db"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
