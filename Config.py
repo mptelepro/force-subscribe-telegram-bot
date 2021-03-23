ee2d82420f073cc306afed280a91550ae4214dfc
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-23 13:07:38 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -13,10 +13,10 @@ class Config():
   else:
     BOT_TOKEN = "1737141128:AAGjU4R2S5CNY7C_i9UrEg5c2vswqK9f5H4"
     DATABASE_URL = "postgres://eyofrnogltxmwz:9f85e8584199a374615a67bfe18f73edc4afaf9451d16eed3aee5b179708589b@ec2-54-243-195-160.compute-1.amazonaws.com:5432/d325npis91qikn"
-    APP_ID = "1741321"
-    API_HASH = "c085ffacfbe7b1473fc1cee1b7e40ca7"
+    APP_ID = "2443183"
+    API_HASH = "66b6799e1b784aff78bad680cd8362db"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
-    SUDO_USERS.append(728771705)
+    SUDO_USERS.append(1575148105)
     SUDO_USERS = list(set(SUDO_USERS))
 
 
